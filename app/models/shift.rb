e2232edbeda109b4b9436ad9bd09a261aa083106
Jeff Wong <jeffmjwong@gmail.com>
2019-02-18 20:57:46 +1000

    Account for double pay rate on sunday and overnights

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -29,12 +29,12 @@ class Shift < ApplicationRecord
       start_time: start.strftime('%I:%M%P'),
       finish_time: finish.strftime('%I:%M%P'),
       break_length: break_length,
-      hours_worked: hours_worked,
+      hours_worked: total_hours_worked,
       shift_cost: shift_cost
     }
   end
 
-  def hours_worked
+  def total_hours_worked
     seconds_in_minute = 60
     minutes_in_hour = 60
 
@@ -43,10 +43,32 @@ class Shift < ApplicationRecord
     ((shift_length_in_minutes - break_length) / minutes_in_hour).round(2)
   end
 
-  def shift_cost
-    hourly_rate = user.organisation.hourly_rate
+  def detailed_hours_worked
+    hours_worked = total_hours_worked
 
-    format('$%.2f', hours_worked * hourly_rate)
+    return { normal: hours_worked, sunday: 0 } if !start.sunday? && !finish.sunday?
+
+    hours_to_midnight = (start.next_day.midnight - start) / 3600
+
+    if start.sunday?
+      return { normal: 0, sunday: hours_worked } if hours_worked < hours_to_midnight
+
+      { normal: hours_worked - hours_to_midnight, sunday: hours_to_midnight }
+    else
+      return { normal: hours_worked, sunday: 0 } if hours_worked < hours_to_midnight
+
+      { normal: hours_to_midnight, sunday: hours_worked - hours_to_midnight }
+    end
+  end
+
+  def shift_cost
+    normal_hourly_rate = user.organisation.hourly_rate
+    sunday_hourly_rate = normal_hourly_rate * 2
+
+    hours = detailed_hours_worked
+    total_cost = hours[:normal] * normal_hourly_rate + hours[:sunday] * sunday_hourly_rate
+
+    format('$%.2f', total_cost)
   end
 
   private
