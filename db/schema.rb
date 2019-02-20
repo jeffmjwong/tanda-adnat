4d835e9baf16aabe2e0a4fcdf66b035f5a6edc6a
Jeff Wong <jeffmjwong@gmail.com>
2019-02-20 18:07:32 +1000

    Add organisation reference to shift model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_094911) do
+ActiveRecord::Schema.define(version: 2019_02_20_080540) do
 
   create_table "organisation_memberships", force: :cascade do |t|
     t.integer "organisation_id"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_02_19_094911) do
     t.integer "break_length", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "organisation_id"
+    t.index ["organisation_id"], name: "index_shifts_on_organisation_id"
     t.index ["user_id"], name: "index_shifts_on_user_id"
   end
 
